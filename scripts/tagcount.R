d3f8e2c8da9efd8d395336c92f2d19bccebc419a
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2019-05-22 14:50:59 +0200

    tagsort.R: successfully reads file, returns sorted tibble

--- scripts/tagcount.R
+++ scripts/tagcount.R
@@ -1,1 +1,16 @@
+# tagcount.R
+#
+# counts occurrences of words in data files,
+# one word per line
+# 
+# data files are specified at the top of the file
+#
+# TODO: accept data file(s) as parameters?
 
+library("tidyverse")
+
+boycott_tags = read_lines("../data/clean_boycottgillette_related_tags")
+boycott_table = table(boycott_tags, deparse.level = 0)
+boycott_sort = sort(boycott_table, decreasing = TRUE)
+boycott_tibble = enframe(boycott_sort, name = "tag", value = "count")
+boycott_tibble
