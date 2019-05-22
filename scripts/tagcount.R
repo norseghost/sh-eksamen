a0244747815831e75fc8e2f0e803456f9ab856ec
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2019-05-22 15:05:31 +0200

    fix incorrect data file reference

--- scripts/tagcount.R
+++ scripts/tagcount.R
@@ -9,7 +9,7 @@
 
 library("tidyverse")
 
-boycott_tags = read_lines("../data/clean_boycottgillette_related_tags")
+boycott_tags = read_lines("../data/clean_#boycottgillette_related_tags")
 boycott_table = table(boycott_tags, deparse.level = 0)
 boycott_sort = sort(boycott_table, decreasing = TRUE)
 boycott_tibble = enframe(boycott_sort, name = "tag", value = "count")
