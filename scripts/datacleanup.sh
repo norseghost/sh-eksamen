0a2b224d771bdef3fe4528cfe0c990393a97b9e4
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2019-05-30 00:56:33 +0200

    Refactor datacleanup.sh
    Added in-script parameters for desired date range.
    
    Also reversed modality; the script now selects files within a desired
    date range, and copies these out to a new directory.
    
    Thus the original dataset is kept intact, to facilitate new filtering
    requirements.

--- scripts/datacleanup.sh
+++ scripts/datacleanup.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+#
+# datacleanup.sh
+# 
+# Usage: datacleanup.sh somedir/
+# Will parse filenames in a directory for date information
+# (in YYYY-MM-DD format, at start of filename) and copies any that
+# fall within begin_ts and end_ts to a new directory
 
-keep_ts=$(date --date="2019-01-13" +%s)
-echo $keep_ts
-mkdir old_${1}
-for file in ${1}*; do
-    file_date=$(echo $file | cut -d '_' -f1 |cut -d '/' -f2 )
-    file_ts=$(date --date=$file_date +%s)
-   # echo $file_date
-    #echo $file_ts
-    if [ "$file_ts" -lt "$keep_ts" ]; then
-        mv "$file" old_${1}
-    fi
-    done
+# TODO:
+# - accept prefix/output dir as parameter
+# - accpt daterange as parameter
+# - only require one of begin/end date
 
-keep_ts=$(date --date="2019-04-08" +%s)
-echo $keep_ts
-mkdir young_${1}
+#Date cutoffs
+
+begin_ts=$(date --date="2019-01-13" +%s)
+end_ts=$(date --date="2019-04-08" +%s)
+
+# backup dir
+# currently made in script exection dir
+# FIXME: be smarter about this
+prefix="filtered"
+if [ ! -d "${prefix}_${1}" ]; then
+   mkdir "${prefix}_${1}"
+fi
+
 for file in ${1}*; do
+    # parse file date
     file_date=$(echo $file | cut -d '_' -f1 |cut -d '/' -f2 )
+    # convert to a timestamp
     file_ts=$(date --date=$file_date +%s)
-   # echo $file_date
-    #echo $file_ts
-    if [ "$file_ts" -gt "$keep_ts" ]; then
-        mv "$file" young_${1}
-    fi
-done
+    # compare file timestamp to desired range
+    if [ "$file_ts" -gt "$begin_ts" -a  "$file_ts" -lt "$end_ts" ]; then
+        # copy files to output dir
+        cp "$file" "${prefix}_${1}"
+     fi
+ done
+
